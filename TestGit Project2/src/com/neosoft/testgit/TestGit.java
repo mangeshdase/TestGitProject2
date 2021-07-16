e2f015c55d7e497aed9ef65c12c5ac52bc39330f
mangeshdase <dasemangesh14@gmail.com>
2021-07-16 18:59:11 +0530

    first commit

---
+++ TestGit Project2/src/com/neosoft/testgit/TestGit.java
@@ -0,0 +1,15 @@
+package com.neosoft.testgit;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String code ="SE";
+		if(code == "SE") {
+			System.out.println("Software Engineer");
+		}
+		else
+			System.out.println("Not a Software Engineer");
+	}
+
+}
